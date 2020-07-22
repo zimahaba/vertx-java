f5f66db1c35e1609ed842672c8f443d73ec2ba34
zimahaba <pmaquitorres@gmail.com>
2020-07-21 22:05:33 -0300

    Docker compose do MonboDB e utilização do MongoClient

--- src/main/java/com/zimahaba/vertx/verticle/MainVerticle.java
+++ src/main/java/com/zimahaba/vertx/verticle/MainVerticle.java
@@ -6,6 +6,7 @@ import io.vertx.core.Promise;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
+import io.vertx.ext.mongo.MongoClient;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.BodyHandler;
@@ -16,10 +17,15 @@ import java.util.stream.Collectors;
 
 public class MainVerticle extends AbstractVerticle {
 
+  private MongoClient mongo;
+
   @Override
   public void start()  {
 
     HttpServer server = vertx.createHttpServer();
+
+    JsonObject mongoConfig = config().put("db_name", "test").put("connection_string", "mongodb://172.20.0.2:27017");
+    mongo = MongoClient.create(vertx, mongoConfig);
 
     // Routes
     Router router = Router.router(vertx);
@@ -30,15 +36,36 @@ public class MainVerticle extends AbstractVerticle {
     server.requestHandler(router).listen(9000);
   }
 
-  private Map<Integer, Entity> map = new HashMap<>();
-
   private void getEntities(RoutingContext context) {
-    context.response().end(new JsonArray(map.values().stream().collect(Collectors.toList())).encodePrettily());
+
+    JsonArray entityList = new JsonArray();
+
+    JsonObject query = new JsonObject();
+    mongo.find("entity", query, res -> {
+      if (res.succeeded()) {
+        for (JsonObject jsonObject: res.result()) {
+          entityList.add(jsonObject);
+        }
+        context.response().end(entityList.encodePrettily());
+      } else {
+        res.cause().printStackTrace();
+      }
+    });
   }
 
   private void addEntity(RoutingContext context) {
     Entity entity = context.getBodyAsJson().mapTo(Entity.class);
-    map.put(entity.getId(), entity);
-    context.response().end(JsonObject.mapFrom(entity).encodePrettily());
+
+    JsonObject jsonEntity = JsonObject.mapFrom(entity);
+    mongo.save("entity", jsonEntity, res -> {
+      if (res.succeeded()) {
+        String id = res.result();
+        System.out.println("Saved entity with id " + id);
+      } else {
+        res.cause().printStackTrace();
+      }
+    });
+
+    context.response().end(jsonEntity.encodePrettily());
   }
 }
